ada71ca45e45a7fd999e69c79d4b27f07bbe653b
vitoriadf <vitoriadf23@gmail.com>
2024-05-27 18:58:54 -0300

    alterações

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <title>Document</title>
+    <title>login</title>
 </head>
 <body>
     <section class="vh-100 corDFundo">
@@ -22,15 +22,15 @@
                                 <form action="verificar/verifLogin.php" method="POST" data-parsley-validate>
 
                                     <?php if(isset($_GET['success']) && $_GET['success'] == 'ok'){ ?>
-                                    <p class="text-center text-success mb-3">Usuário cadastrado com sucesso!</p>
+                                    <p class="text-center mb-3 conteudoInputSucesso">Usuário cadastrado com sucesso!</p>
                                     <?php } ?>
 
                                     <?php if(isset($_GET['error']) && $_GET['error'] === 'erro'){ ?>
-                                    <p class="text-center text-white mb-3 " role="alert">Login ou senha incorretos!</p>
+                                    <p class="text-center  mb-3 conteudoInputError"  role="alert">Login ou senha incorretos!</p>
                                     <?php } ?>
 
                                     <?php if(isset($_GET['error']) && $_GET['error'] === 'emBranco'){ ?>
-                                    <p class="text-center text-white mb-3" role="alert">Preencha os campos!</p>
+                                    <p class="text-center  mb-3 conteudoInputError" role="alert">Preencha os campos!</p>
                                     <?php } ?>
 
                                     <div class="form-outline mb-3 divsInputs" >
